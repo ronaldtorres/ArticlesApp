48584b0e901701d5521acfb1cfb7f73e21dc8c5a
Ronald Torres <ronald26d18@gmail.com>
2022-10-16 00:48:06 -0500

    clean some files

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,6 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
